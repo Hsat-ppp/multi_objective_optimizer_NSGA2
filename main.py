7cbe57551bcf6495e0fd4abcba547f7dae0d7c52
hayaho_sato <hsatoo10@gmail.com>
2022-06-17 19:06:20 +0900

    added args / progress bar

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import logging.config
 
 import single_objective_optimizer.controller.controller
 
+# load logging config
 with open('log_config.json', 'r') as f:
     log_conf = json.load(f)
 logging.config.dictConfig(log_conf)
 
+# run optimization
 single_objective_optimizer.controller.controller.optimize()
--- single_objective_optimizer/model/common_settings.py
+++ single_objective_optimizer/model/common_settings.py
@@ -1,9 +1,6 @@
 n = 2
-generation_max = 100
 
 INF = 1E+30
 
 plot_range = [-10, 10]
 width_for_plot = 0.1
-
-result_dir = 'result/'
